763477fdebf6d6de6784e011a806683ec615f4b2
Luisina Martinez <luuumartinez13@gmail.com>
2023-09-15 12:18:39 -0300

    imagenes

---
+++ src/rutas/Rutas.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom"
+import Registro from "../pages/registro/Registro"
+import FormAgregarProd from "../components/productos/FormAgregarProd";
+import Tienda from "../pages/tienda/Tienda"
+import Administracion from "../pages/administracion/Administracion";
+import Login from "../components/login/Login";
+import Inicio from "../pages/inicio/Inicio";
+
+
+const Rutas = () =>{
+    return(
+        <>
+        <Routes>
+        <Route path="/" element={<Inicio />} />
+            <Route path="/registro" element={<Registro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/agregarproductos" element={<FormAgregarProd />} />
+            <Route path="/tienda" element={<Tienda />} />
+            <Route path="/administracion" element={<Administracion />} />
+
+        </Routes>
+
+        </>
+    )
+}
+
+export default Rutas;
